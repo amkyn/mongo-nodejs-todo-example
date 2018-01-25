b2bf6bb5a1d41d8dd7954931e20672265a3a23b5
amkyn <msmitran@gmail.com>
2018-01-25 06:29:23 +0100

    clever cloud uri

--- db.js
+++ db.js
@@ -11,4 +11,4 @@ mongoose.model( 'Todo', Todo );
 
 // Clever Cloud connection
 // mongoose.connect( 'mongodb://db_username:db_password@db_host/db_name' );
-mongoose.connect( 'mongodb://localhost/nodemongo' );
+mongoose.connect( 'mongodb://upi8z7ecoie7jda:9uRjTbB2lr6RiTdyIQpn@bubk3zzp6mrilrc-mongodb.services.clever-cloud.com:27017/bubk3zzp6mrilrc' );
